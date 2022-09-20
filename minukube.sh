22e91faae0a8036a7915d8130902a48ca04e423a
sahu04 <105695204+sahu04@users.noreply.github.com>
2022-09-20 12:05:48 +0530

    Create minukube.sh

---
+++ minukube.sh
@@ -0,0 +1,13 @@
+sudo su
+
+Now install docker
+
+sudo apt update && apt -y install docker.io
+
+install Kubectl
+
+curl -LO https://storage.googleapis.com/kubern... -s https://storage.googleapis.com/kubern... && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+
+install Minikube
+
+curl -Lo minikube https://storage.googleapis.com/miniku... && chmod +x minikube && sudo mv minikube /usr/local/bin/
